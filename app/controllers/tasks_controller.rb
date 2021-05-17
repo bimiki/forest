2c37e037bbda9198ba7d04febd866127cb88c645
bimiki <noahtamulonis@gmail.com>
2021-05-17 15:18:07 -0400

    lunch time

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,7 +51,7 @@ class TasksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_notes
-      @notes = Note.find(params[:notes_id])
+      @notes = current_user.notes.find(params[:notes_id])
     end
 
     def set_task
