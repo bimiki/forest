2a5a9d9896415b8031c7c5771fd527b24fac1a6b
bimiki <noahtamulonis@gmail.com>
2021-05-28 11:24:11 -0400

    fuction update

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,9 +2,16 @@ class Note < ApplicationRecord
   belongs_to :user
   acts_as_votable cacheable_strategy: :update_columns
 
+  has_many :votes
 
   def enbedable_url
     youtube_id = description.split("?v=").last
     "https://www.youtube.com/embed/#{youtube_id}"
   end
+
 end
+@note = Note.new(:name => 'my post!')
+@note.save
+
+@note.liked_by @user
+@note.votes_for.size # => 1
