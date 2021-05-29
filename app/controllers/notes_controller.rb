01f08182e3e09c4c1960b3dbb16add792ba51f90
bimiki <noahtamulonis@gmail.com>
2021-05-29 10:23:18 -0400

    notes listed by likes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,7 +4,7 @@ class NotesController < ApplicationController
   skip_before_action :verify_authenticity_token
   # GET /notes or /notes.json
   def index
-    @notes = current_user.notes
+    @notes = Note.all.order(cached_votes_score: :desc)
   end
 
   def upvote
